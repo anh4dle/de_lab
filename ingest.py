32205f3b4710880f8465361115fcebc3e02edc2f
Anh Le <anh4dle@gmail.com>
2025-06-23 10:15:11 +0700

    update

--- ingest.py
+++ ingest.py
@@ -1,17 +1,19 @@
-import requests
-import asyncio
-import aiohttp
+import asyncio 
+import aiohttp #Use this instead of requests because it is non-blocking
 import os
 
 base_url = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
 
 async def download(session, download_url):
-    async with session.get(download_url) as res:
+    async with session.get(download_url, ssl=False) as res:
         if res.status == 200:
-            first_dash = download_url.rfind("/")
-            filename = download_url[first_dash + 1:]
-            print(filename)
-
+            last_dash = download_url.rfind("/")
+            filename = download_url[last_dash + 1:]
+            print(f"Writing {filename}")
+            with open(filename, "wb") as f:
+                async for chunk in res.content.iter_chunked(1024):
+                    f.write(chunk)
+            
 #Make dir to store downloaded data, make dir based on year
 def create_directory(dir_name):
     if not os.path.exists(dir_name):
@@ -24,11 +26,10 @@ def month_to_string(month):
         month = str(month)
     return month
 
-async def main():
-    year = 2020
+async def main(current_year, end_year):
     urls = []
-    while year < 2025:
-        create_directory(str(year))
+    while current_year < end_year:
+        create_directory(str(current_year))
         month = 1
         while month < 13:
             month_str = month_to_string(month)
@@ -44,4 +45,6 @@ async def main():
         for result in results:
             print(result)
 
-asyncio.run(main())
+current_year = 2020
+end_year = 2025
+asyncio.run(main(current_year, end_year))
