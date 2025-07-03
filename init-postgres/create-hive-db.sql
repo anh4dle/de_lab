06a3663f53cf070aa4337d620e3922d37ea35e63
Anh Le <anh4dle@gmail.com>
2025-07-03 13:52:20 +0700

    update

---
+++ init-postgres/create-hive-db.sql
@@ -0,0 +1,6 @@
+-- Create Hive Metastore database
+CREATE DATABASE hive_metastore;
+
+-- (Optional) create a separate user for Hive if desired:
+-- CREATE USER hive_user WITH PASSWORD 'hive_password';
+-- GRANT ALL PRIVILEGES ON DATABASE hive_metastore TO hive_user;
