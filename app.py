ba5ba3a39430bcea78c7fb1a1d44065dbc88ae53
Anh Le <anh4dle@gmail.com>
2025-07-04 17:12:35 +0700

    update

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
 import os
-import dotenv
 import asyncio
 import csv
+import io
 import asyncio
 import aiohttp  # Use this instead of requests because it is non-blocking
 from pathlib import Path
 
 from datetime import datetime
 import pytz
-from minio import MinIO
+from utils.minio_utils import get_minio_client, upload_stream_obj, get_downloaded_files_by_year
+
 # base_url = "https://www.nyc.gov/site/tlc/about/tlc-trip-record-data.page"
 tz = pytz.timezone("Asia/Ho_Chi_Minh")
-
-
-def get_minio_client(MINIO_ACCESS, MINIO_PASS):
-    minio_client = MinIO(
-        "localhost:9000",
-        access_key=MINIO_ACCESS,
-        secret_key=MINIO_PASS,
-        secure=False
-    )
 
 
 def log_failed_download(filename, error, csv_writer):
@@ -36,35 +28,39 @@ def check_download_complete(total_bytes, res, filename):
     return True
 
 
-async def download(minio, session, dir_path, download_url, retry_time, error_file):
+async def write_to_bytesIO(response):
+    data = io.BytesIO()
+    async for chunk in response.content.iter_chunked(1024):
+        data.write(chunk)
+    data.seek(0)
+    return data
+
+
+async def upload_to_minio(minio, minio_upload_path, data):
+    try:
+        upload_stream_obj(minio, bucket_name='lake',
+                          object_name=minio_upload_path, data=data)
+        print("Upload successful!")
+    except Exception as e:
+        raise Exception(f"Upload failed: {e}")
+
+
+async def download_and_upload(minio, aiohttp_session, dir_path, download_url, retry_time, error_logs):
     last_dash = download_url.rfind("/")
     filename = download_url[last_dash + 1:]
     for attemp in range(retry_time + 1):
         try:
             # Download bytes and write to a file
-            async with session.get(download_url, ssl=False, stream=True) as res:
+            async with aiohttp_session.get(download_url, ssl=False) as res:
                 if res.status == 200:
                     print(f"Writing data to {filename}")
-                    file_path = Path(dir_path) / filename
-                    try:
-                        minio.upload_fileobj(
-                            res.raw, 'raw-zone', file_path)
-                        print("Upload successful!")
-                    except Exception as e:
-                        raise Exception(f"Upload failed: {e}")
-                    # with open(file_path, "wb") as f:
-                    #     total_bytes = 0
-                    #     async for chunk in res.content.iter_chunked(1024):
-                    #         f.write(chunk)
-                    #         total_bytes += len(chunk)
-                    #     if check_download_complete(total_bytes, res, filename):
-                    #         return
-                    #     else:
-                    #         log_failed_download(
-                    #             filename, 'Incomplete download for file', error_file)
-                    # file_path.unlink()
+                    data = await write_to_bytesIO(res)
+
+                    minio_upload_path = Path(dir_path) / filename
+                    upload_to_minio(minio, minio_upload_path, data)
+
         except Exception as e:
-            log_failed_download(filename, str(e), error_file)
+            log_failed_download(filename, str(e), error_logs)
         if attemp < retry_time:
             await asyncio.sleep(2)
         else:
@@ -80,28 +76,29 @@ def convert_month_to_string(month):
     return month
 
 
-async def extract(minio, current_year, end_year, csv_writer, downloaded_files):
+async def extract(minio, current_year, end_year, error_logs):
     urls = []
     retry_times = 0
+
     while current_year < end_year:
-        dir_path = Path("data") / str(current_year)
-        if not Path(dir_path).exists():
-            Path(dir_path).mkdir(parents=True)
+        minio_upload_path = str(Path('lake/parquetfiles') / str(current_year))
         month = 1
+        downloaded_files = get_downloaded_files_by_year(
+            minio, 'lake', current_year)
         while month < 3:
             month_str = convert_month_to_string(month)
             download_url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{current_year}-{month_str}.parquet"
             last_dash = download_url.rfind("/")
             filename = download_url[last_dash + 1:]
-
-            if filename not in downloaded_files:
-                print(f"Downloading {filename} {download_url}")
-                urls.append((dir_path, download_url))
+            filepath = f"{str(current_year)} / {download_url[last_dash + 1:]}"
+            if filepath not in downloaded_files:
+                print(f"Downloading {filename}")
+                urls.append((minio_upload_path, download_url))
             month += 1
         current_year += 1
 
-    async with aiohttp.ClientSession() as session:
-        tasks = [download(minio, session, dir_path, url, retry_times, csv_writer)
+    async with aiohttp.ClientSession() as aiohttp_session:
+        tasks = [download_and_upload(minio, aiohttp_session, dir_path, url, retry_times, error_logs)
                  for dir_path, url in urls]
         await asyncio.gather(*tasks)
 
@@ -111,19 +108,20 @@ def main():
     if not Path('logs').exists():
         Path('logs').mkdir(parents=True)
 
-    downloaded_files = {f.name for f in Path('data').rglob('*') if f.is_file()}
+    # Get downloaded files
+
     minio_access, minio_pass = os.getenv(
         'MINIO_ROOT_USER'), os.getenv('MINIO_ROOT_PASSWORD')
     minio = get_minio_client(minio_access, minio_pass)
     with open(file_path, 'a') as f:
-        csv_writer = csv.writer(f)
+        error_logs = csv.writer(f)
         if Path(file_path).stat().st_size == 0:
             header = ['File', 'Error', 'Time']
-            csv_writer.writerow(header)
+            error_logs.writerow(header)
         current_year = 2020
         end_year = 2022
         asyncio.run(extract(minio, current_year, end_year,
-                    csv_writer, downloaded_files))
+                    error_logs))
 
 
 main()
--- utils/minio_utils.py
+++ utils/minio_utils.py
@@ -1,6 +1,7 @@
 from minio import Minio
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
 
 def get_minio_client(minio_access: str, minio_pass: str):
@@ -13,17 +14,30 @@ def get_minio_client(minio_access: str, minio_pass: str):
     return client
 
 
+def get_downloaded_files_by_year(minio, bucket, year):
+    prefix = f"{year}/"  # Ensure it's a string with a trailing slash
+
+    objects = minio.list_objects(bucket, prefix=prefix, recursive=True)
+    result = [object.object_name for object in objects]
+    return result
+
+
 def create_bucket(minio_client: Minio, bucket_name: str):
     if not minio_client.bucket_exists(bucket_name):
         minio_client.make_bucket(bucket_name)
+
+
+def upload_stream_obj(minio_client: Minio, bucket_name: str, object_name: str, data):
+    minio_client.put_object(bucket_name, object_name, data)
 
 
 def main():
     minio_access, minio_pass = os.getenv(
         'MINIO_ROOT_USER'), os.getenv('MINIO_ROOT_PASSWORD')
     minio_client = get_minio_client(minio_access, minio_pass)
-
+    bucket = 'lake'
     create_bucket(minio_client, 'lake')
+    print(get_downloaded_files_by_year(minio_client, bucket, 2020))
 
 
 main()
